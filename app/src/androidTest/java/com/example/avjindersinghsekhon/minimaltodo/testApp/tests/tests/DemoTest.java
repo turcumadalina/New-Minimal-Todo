9989c6746acc327de20be0f80fca5c1e902e76e6
P6377\cezar.iordache <cezar.iordache@softvision.ro>
2018-05-08 10:13:29 +0300

    Added new test

--- app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/testApp/tests/tests/DemoTest.java
+++ app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/testApp/tests/tests/DemoTest.java
@@ -1,10 +1,12 @@
 package com.example.avjindersinghsekhon.minimaltodo.testApp.tests.tests;
 
 import com.example.avjindersinghsekhon.minimaltodo.R;
+
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
@@ -30,24 +32,31 @@ public class DemoTest extends EspressoTestBase {
         onView(withId(R.id.makeToDoFloatingActionButton)).perform(click());
 
         onView(allOf(withId(R.id.toDoListItemTextview), withText("B"), isCompletelyDisplayed())).check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.toDoListItemTextview), withText("B"),isCompletelyDisplayed())).perform(swipeLeft());
+
+        onView(allOf(withId(R.id.toDoListItemTextview), withText("A"),isCompletelyDisplayed())).perform(swipeLeft());
     }
 
     @Test
-    public void testAddItems2() {
+    public void Remind(){
         onView(withId(R.id.addToDoItemFAB)).perform(click());
 
-        onView(withId(R.id.userToDoEditText)).perform(typeText("a"));
+        onView(withId(R.id.userToDoEditText)).perform(typeText("Remind Me"));
+
+        onView(withId(R.id.toDoHasDateSwitchCompat)).perform(click());
+
+        onView(withId(R.id.newTodoDateEditText)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.newTodoTimeEditText)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.newToDoDateTimeReminderTextView)).check(matches(isDisplayed()));
 
         onView(withId(R.id.makeToDoFloatingActionButton)).perform(click());
-    }
 
-    @Test
-    public void testAddItems3() {
-        onView(withId(R.id.addToDoItemFAB)).perform(click());
+        onView(allOf(withId(R.id.todoListItemTimeTextView))).check(matches(isDisplayed()));
 
-        onView(withId(R.id.userToDoEditText)).perform(typeText("a"));
-
-        onView(withId(R.id.makeToDoFloatingActionButton)).perform(click());
+        onView(withId(R.id.toDoListItemTextview)).perform(swipeLeft());
     }
 
 }
